d9a0cd783bc31e4d3ae54e6790adc92af14387c1
Denys Tsymbalenko <d.tsymbalenko@astoundcommerce.com>
2020-11-01 18:59:04 +0200

    Add Portfolio section

--- src/App.jsx
+++ src/App.jsx
@@ -117,16 +117,7 @@ const Portfolio = () => (
         >
           {PORTFOLIO.map(item => (
             <SwiperSlide className="portfolio-item" key={item.title}>
-              <PortfolioItem {...item}
-                popupSiteLink="https://carswork.com"
-                popupSiteLogo="/assets/imgs/laptop2.png"
-                popupMissionTextFirst="We’ll help you grow and enhance your business using best practices staring from usage of modern technologies and ending with best Agile practices for organizational needs."
-                popupMissionTextSecond="We’ll help you grow and enhance your business using best practices staring from usage of modern technologies and ending with best Agile practices for organizational needs."
-                popupTechnologiesDB="PostgreSQL / MongoDB / MySQL / DynamoDb / Elasticsearch / Redis"
-                popupTechnologiesGeneral="OOP & OOD / TDD & BDD / API (REST API & Postman & Swagger & API Doc)"
-                popupTechnologiesFE="Angular (Typescript) & React & Net.JS Frameworks / HTML / CSS (SCSS, LESS, bootstrap, angular material), Javascript (ES6 - ES9)"
-                popupTechnologiesBE="Node.js (Koa, express, meteor, hapi, sails, nestjs)"
-              />
+              <PortfolioItem {...item} />
             </SwiperSlide>
           ))}
 
