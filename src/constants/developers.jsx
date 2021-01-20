c1b299193fe93436457403fbe892d985796ab2df
DESKTOP-Q7BB3CC\roman <roman.prystojnyj@gmail.com>
2021-01-20 18:05:54 +0200

    Fixed format img

--- src/constants/developers.jsx
+++ src/constants/developers.jsx
@@ -1,5 +1,35 @@
 const DEVELOPERS = [
   {
+    img: '/assets/imgs/devs/ihor_z.png',
+    name: 'Ihor Zhariy',
+    profession: 'Business Owner'
+  },
+  {
+    img: '/assets/imgs/devs/roma_h.jpg',
+    name: 'Roma Humenuk',
+    profession: 'Product Manager'
+  },
+  {
+    img: '/assets/imgs/devs/semen_y.png',
+    name: 'Semen Yevmenenko',
+    profession: 'Solution Architect'
+  },
+  {
+    img: '/assets/imgs/devs/maxim_p.jpg',
+    name: 'Maxim Polishchuk',
+    profession: 'Chief Executive Officer'
+  },
+  {
+    img: '/assets/imgs/devs/bogdan_d.png',
+    name: 'Bogdan Dereviaga',
+    profession: 'Senior Back-End Developer'
+  },
+  {
+    img: '/assets/imgs/devs/sergei_s.png',
+    name: 'Sergey Shchur',
+    profession: 'Project Manager'
+  },
+  {
     img: '/assets/imgs/devs/veronika_t.jpg',
     name: 'Veronika Tokarenko',
     profession: 'Project Manager'
@@ -10,11 +40,20 @@ const DEVELOPERS = [
     profession: 'AQA Java & Java Backend'
   },
   {
+    img: '/assets/imgs/devs/igor_n.jpg',
+    name: 'Igor Nerovnyi',
+    profession: 'Middle Front-End Developer'
+  },
+  {
+    img: '/assets/imgs/devs/nikolay_s.jpg',
+    name: 'Nikolay Strashok',
+    profession: 'Middle Front-End Developer'
+  },
+  {
     img: '/assets/imgs/devs/oles_h.jpg',
     name: 'Oles Hrytseniuk',
     profession: 'Middle Full-Stack Developer'
   },
-
   {
     img: '/assets/imgs/devs/pavlo_y.png',
     name: 'Pavlo Yakhontov',
@@ -49,6 +88,21 @@ const DEVELOPERS = [
     img: '/assets/imgs/devs/stanislav_d.jpg',
     name: 'Stanislav Domansky',
     profession: 'Strong Junior Full-Stack Developer'
+  },
+  {
+    img: '/assets/imgs/devs/nata_p.jpg',
+    name: 'Nata Pavlovska',
+    profession: 'UI Designer, Illustrator'
+  },
+  {
+    img: '/assets/imgs/devs/denis_t.jpg',
+    name: 'Denis Tsymbalenko',
+    profession: 'Junior Front-End Developer'
+  },
+  {
+    img: '/assets/imgs/devs/kyrylo_p.png',
+    name: 'Kyrylo Potanin',
+    profession: 'Junior Full-stack Developer'
   },
 ];
 
