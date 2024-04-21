c4105f2d3e35690fdb3f613e04ceb7c219127549
Pavel Nezdjur <pavel.nezdjur@gmail.com>
2024-04-21 23:38:42 +0300

    the users of the team are changed

--- src/constants/developers.jsx
+++ src/constants/developers.jsx
@@ -10,45 +10,66 @@ const DEVELOPERS = [
     profession: 'Chief Executive Officer'
   },
   {
-    img: '/assets/imgs/devs/bogdan_d.png',
-    name: 'Bogdan Dereviaga',
-    profession: 'Senior Back-End Developer'
-  },
-  {
     img: '/assets/imgs/devs/sergei_s.png',
     name: 'Sergey Shchur',
     profession: 'Project Manager'
   },
   {
-    img: '/assets/imgs/devs/veronika_t.jpg',
-    name: 'Veronika Tokarenko',
+    img: '/assets/imgs/devs/gleb_k.jpg',
+    name: 'Gleb Kirsenko',
     profession: 'Project Manager'
-  },
-  {
-    img: '/assets/imgs/devs/oksana_k.jpg',
-    name: 'Oksana Fedoryshyna',
-    profession: 'Project Manager'
-  },
-  {
-    img: '/assets/imgs/devs/pavlo_m.jpg',
-    name: 'Pavlo Makukha',
-    profession: 'Full Stack Node.js Engineer'
   },
   {
     img: '/assets/imgs/devs/ivan_sh.jpg',
     name: 'Ivan Shevchenko',
-    profession: 'Strong Middle Full-Stack Developer'
+    profession: 'Middle Full-Stack Developer'
   },
   {
     img: '/assets/imgs/devs/roman_p.jpg',
     name: 'Roman Prystoinyi',
-    profession: 'Strong Middle Full-Stack Developer'
+    profession: 'Middle Full-Stack Developer'
   },
   {
     img: '/assets/imgs/devs/ivan_m.jpg',
     name: 'Ivan Melnychuk',
+    profession: 'Middle Full-Stack Developer'
+  },
+  {
+    img: '/assets/imgs/devs/anatoliy_k.jpg',
+    name: 'Anatoliy Kobzar',
+    profession: 'Junior Full-Stack Developer'
+  },
+  {
+    img: '/assets/imgs/devs/lubov_r.jpg',
+    name: 'Lubov Ridkokasha',
+    profession: 'Junior Full-Stack Developer'
+  },
+  {
+    img: '/assets/imgs/devs/pavel_n.jpg',
+    name: 'Pavel Nezdjur',
     profession: 'Junior Full-Stack Developer'
   },
 ];
 
 export default DEVELOPERS;
+
+// {
+//   img: '/assets/imgs/devs/bogdan_d.png',
+//   name: 'Bogdan Dereviaga',
+//   profession: 'Senior Back-End Developer'
+// },
+// {
+//   img: '/assets/imgs/devs/veronika_t.jpg',
+//   name: 'Veronika Tokarenko',
+//   profession: 'Project Manager'
+// },
+// {
+//   img: '/assets/imgs/devs/oksana_k.jpg',
+//   name: 'Oksana Fedoryshyna',
+//   profession: 'Project Manager'
+// },
+// {
+//   img: '/assets/imgs/devs/pavlo_m.jpg',
+//   name: 'Pavlo Makukha',
+//   profession: 'Full Stack Node.js Engineer'
+// },
